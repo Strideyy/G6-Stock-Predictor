2b9d48c8193661ee4181baca2dbc055d33593eac
Strideyy <katastrophe599@live.co.uk>
2021-03-04 21:28:30 +0000

    Version 0.2.0

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # G6-Stock-Predictor
 
 
-# Initial Commit: 
+## Initial Commit: 
 - Opening CSV file as Pandas DataFrame object (https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html).
 - Shows stock high prices over time.
 
-# Version 0.1.0:
+## Version 0.1.0:
 - Line graph for visualisation purposes
 - Normalise data
 - Create 70-30 train-test data split
+
+## Version 0.2.0:
+- Function for creating an array where X is the price of one share, and Y is the price of that share the next day.
+
--- main.py
+++ main.py
@@ -2,40 +2,51 @@ import os
 import pandas as pd
 import numpy
 import matplotlib.pyplot as plt
-import math
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
-# Imports for future use
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+import math
+from sklearn.metrics import mean_squared_error
 
-ROOT_PATH = "C:\\Users\\John\\Downloads\\archive\\stocks-50\\" # Locally stored for now, until I find a proper remote host
 
-numpy.random.seed(7) 
+ROOT_PATH = "C:\\Users\\John\\Downloads\\archive\\stocks\\"
+# Locally stored for now, until I find a proper remote host
+
+numpy.random.seed(7)
+
 
 def load_stock_data(root_path=ROOT_PATH):
-    csv_path = os.path.join(root_path, "A.csv") 
-    return pd.read_csv(csv_path, usecols=[1,2,3,4]) # Columns: Open, High, Low, Close
+    csv_path = os.path.join(root_path, "GME.csv")
+    return pd.read_csv(csv_path, usecols=[4])  # Column: Close
+
 
 stock_data = load_stock_data()
 
-## Requires Jupyter ##
-plt.figure(figsize = (18,9)) 
-plt.plot(range(stock_data.shape[0]),(stock_data['Low']+stock_data['High'])/2.0)
-#plt.xticks(range(0,stock_data.shape[0],500),stock_data['Date'].loc[::500],rotation=45)
-#plt.xlabel('Date',fontsize=18)
-#plt.ylabel('Mid Price',fontsize=18)
-plt.show()
-####
+# Requires Jupyter
+#plt.figure(figsize=(18, 9))
+#plt.plot(range(stock_data.shape[0]), (stock_data['Low']+stock_data['High'])/2.0)
+#plt.xticks(range(0, stock_data.shape[0], 500), stock_data['Date'].loc[::500], rotation=45)
+#plt.xlabel('Date', fontsize=18)
+#plt.ylabel('Mid Price', fontsize=18)
+#plt.show()
 
 dataset = stock_data.values
 dataset = dataset.astype('float32')
 
-scaler = MinMaxScaler(feature_range=(0,1)) # Normalise for better performance
+scaler = MinMaxScaler(feature_range=(0, 1))  # Normalise for better performance
 dataset = scaler.fit_transform(dataset)
 
-train_size = int(len(dataset) * 0.70) # Training set size: 70%
-test_size = len(dataset) - train_size # Test set size: remaining 30%
-train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-print("Training set size:", len(train), "\nTest set size:", len(test)) 
+train_size = int(len(dataset) * 0.70)  # Training set size: 70%
+test_size = len(dataset) - train_size  # Test set size: remaining 30%
+train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
+print("Training set size:", len(train), "\nTest set size:", len(test))
+
+
+def create_dataset(dataset, look_back=1):
+    data_x, data_y = [], []
+    for i in range(len(dataset)-look_back-1):
+        a = dataset[i:(i+look_back), 0]
+        data_x.append(a)
+        data_y.append(dataset[i+look_back, 0])
+    return numpy.array(data_x), numpy.array(data_y)
