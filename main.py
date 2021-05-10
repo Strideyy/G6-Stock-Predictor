43e41480c9f430dd17e8427864c4d3ce7e0fe81f
Strider <37343347+Strideyy@users.noreply.github.com>
2021-05-10 13:56:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,26 +10,30 @@ import math
 from sklearn.metrics import mean_squared_error
 
 
-ROOT_PATH = "C:\\Users\\John\\Downloads\\archive\\stocks\\"
+ROOT_PATH = "C:\\Users\\John\\Desktop\\University\\Second Year\\Team Software Engineering\\Assessment 3\\External\\archive\\stocks"
 # Locally stored for now, until I find a proper remote host
 
 numpy.random.seed(7)
 
 
 def load_stock_data(root_path=ROOT_PATH):
-    csv_path = os.path.join(root_path, "GME.csv")
-    return pd.read_csv(csv_path, usecols=[4])  # Column: Close
+    csv_path = os.path.join(root_path, "ACAM.csv")
+    return pd.read_csv(csv_path, usecols=['Close'])
+
+
+look_back = 1
+
+
+def create_dataset(dataset, look_back):
+    data_x, data_y = [], []
+    for i in range(len(dataset)-look_back-1):
+        a = dataset[i:(i+look_back), 0]
+        data_x.append(a)
+        data_y.append(dataset[i+look_back, 0])
+    return numpy.array(data_x), numpy.array(data_y)
 
 
 stock_data = load_stock_data()
-
-# Requires Jupyter
-#plt.figure(figsize=(18, 9))
-#plt.plot(range(stock_data.shape[0]), (stock_data['Low']+stock_data['High'])/2.0)
-#plt.xticks(range(0, stock_data.shape[0], 500), stock_data['Date'].loc[::500], rotation=45)
-#plt.xlabel('Date', fontsize=18)
-#plt.ylabel('Mid Price', fontsize=18)
-#plt.show()
 
 dataset = stock_data.values
 dataset = dataset.astype('float32')
@@ -42,11 +46,54 @@ test_size = len(dataset) - train_size  # Test set size: remaining 30%
 train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
 print("Training set size:", len(train), "\nTest set size:", len(test))
 
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
 
-def create_dataset(dataset, look_back=1):    # look_back=1 : This value refers to how far the algorithm "looks back" to predict the next data point
-    data_x, data_y = [], []                  # Since stock price doesn't change drastically overnight, looking back one day is sensible 
-    for i in range(len(dataset)-look_back-1):
-        a = dataset[i:(i+look_back), 0]
-        data_x.append(a)
-        data_y.append(dataset[i+look_back, 0])
-    return numpy.array(data_x), numpy.array(data_y)
+trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+
+
+def create_model(look_back):
+    model = Sequential()
+    model.add(LSTM(4, input_shape=(1, look_back)))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model 
+
+
+def train_test(look_back, trainX, testX, trainY, testY, dataset):
+    model = create_model(look_back)
+    model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+    
+    trainPredict = model.predict(trainX)
+    testPredict = model.predict(testX)
+
+    trainPredict = scaler.inverse_transform(trainPredict)
+    trainY = scaler.inverse_transform([trainY])
+    testPredict = scaler.inverse_transform(testPredict)
+    testY = scaler.inverse_transform([testY])
+
+    trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:, 0]))
+    print('Train Score: %.2f RMSE' % (trainScore))
+    testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:, 0]))
+    print('Test Score: %.2f RMSE' % (testScore))
+
+    plot(trainPredict, testPredict, dataset)
+
+
+def plot(trainPredict, testPredict, dataset):
+    trainPredictPlot = numpy.empty_like(dataset)
+    trainPredictPlot[:, :] = numpy.nan
+    trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+    # shift test predictions for plotting
+    testPredictPlot = numpy.empty_like(dataset)
+    testPredictPlot[:, :] = numpy.nan
+    testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+    # plot baseline and predictions
+    plt.plot(scaler.inverse_transform(dataset))
+    plt.plot(trainPredictPlot)
+    plt.plot(testPredictPlot)
+    plt.show()
+
+
+train_test(look_back, trainX, testX, trainY, testY, dataset)
