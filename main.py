b5a2d3343c110f0bdc5957d7cfaa6c7e811b6127
Strider <37343347+Strideyy@users.noreply.github.com>
2021-05-13 10:18:07 +0100

    Update main.py
    Comments and consistency update

--- main.py
+++ main.py
@@ -13,22 +13,23 @@ from sklearn.metrics import mean_squared_error
 
 def load_stock_data(user_choice):
     root_path = "https://raw.githubusercontent.com/Strideyy/G6-Stock-Predictor/master/sample/"
-    csv_path = os.path.join(root_path, user_choice + ".csv") # IMPORTANT: MUST be a valid .csv located in /master/sample/
+    csv_path = os.path.join(root_path, user_choice + ".csv")
     print(csv_path)
+    
     return pd.read_csv(csv_path, usecols=['Close'])
 
 
-
 numpy.random.seed(7) # Fix seed for reporoducibility 
-look_back = 1
+look_back = 1 # Number of time-steps to learn from
 
-# Converting dataset to matrix, X = t (
+# Converting dataset to matrix, X=t (sample data at current time step) Y=t (x+1 sample at next time step)
 def create_dataset(dataset, look_back):
     data_x, data_y = [], []
     for i in range(len(dataset)-look_back-1):
         a = dataset[i:(i+look_back), 0]
         data_x.append(a)
         data_y.append(dataset[i+look_back, 0])
+    
     return numpy.array(data_x), numpy.array(data_y)
 
 
@@ -38,7 +39,7 @@ def split_data(user_choice):
     dataset = stock_data.values
     dataset = dataset.astype('float32')
 
-    scaler = MinMaxScaler(feature_range=(0, 1))  # Normalise for better performance
+    scaler = MinMaxScaler(feature_range=(0, 1))  # Scaler to normalise for better performance
     dataset = scaler.fit_transform(dataset)
 
     train_size = int(len(dataset) * 0.70)  # Training set size: 70%
@@ -46,58 +47,54 @@ def split_data(user_choice):
     train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
     print("Training set size:", len(train), "\nTest set size:", len(test))
 
-    trainX, trainY = create_dataset(train, look_back)
-    testX, testY = create_dataset(test, look_back)
+    train_x, train_y = create_dataset(train, look_back)
+    test_x, test_y = create_dataset(test, look_back)
 
-    trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
-    testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+    train_x = numpy.reshape(train_x, (train_x.shape[0], train_x.shape[1], 1)) # One time step per sample of data
+    test_x = numpy.reshape(test_x, (test_x.shape[0], test_x.shape[1], 1))
 
-    return trainX, testX, trainY, testY, dataset, scaler
+    return train_x, test_x, train_y, test_y, dataset, scaler
 
 
 def create_model(look_back):
     model = Sequential()
-
-    model.add(LSTM(units = 4, input_shape=(1, look_back)))
-
-    model.add(Dense(1))
-
+    model.add(LSTM(units = 4, input_shape=(1, look_back))) # Neural layer (4 neurons)
+    model.add(Dense(1)) # Output layer
     model.compile(loss='mean_squared_error', optimizer='adam')
+    
     return model 
 
 
-def train_test(look_back, trainX, testX, trainY, testY, dataset, scaler):
+def train_test(look_back, train_x, test_x, train_y, test_y, dataset, scaler):
     model = create_model(look_back)
-    model.fit(trainX, trainY, epochs=1, batch_size=1, verbose=0) #RAISE EPOCHS BEFORE SUBMISSION
+    # Epochs = Number of times model is trained, more epochs is longer runtime
+    model.fit(train_x, train_y, epochs=30, batch_size=1, verbose=0) 
     
-    trainPredict = model.predict(trainX)
-    testPredict = model.predict(testX)
+    train_predict= model.predict(train_x)
+    test_predict = model.predict(test_x)
+    # The data needs to be in the same format as the original to get accurate RMSE
+    train_predict = scaler.inverse_transform(train_predict)
+    train_y = scaler.inverse_transform([train_y])
+    test_predict = scaler.inverse_transform(test_predict)
+    test_y = scaler.inverse_transform([test_y])
 
-    trainPredict = scaler.inverse_transform(trainPredict)
-    trainY = scaler.inverse_transform([trainY])
-    testPredict = scaler.inverse_transform(testPredict)
-    testY = scaler.inverse_transform([testY])
+    # Calculate RMSE for model fit evaluation
+    train_score = math.sqrt(mean_squared_error(train_y[0], train_predict[:, 0]))
+    print('Train Score: %.2f RMSE' % (train_score))
+    test_score = math.sqrt(mean_squared_error(test_y[0], test_predict[:, 0]))
+    print('Test Score: %.2f RMSE' % (test_score))
 
-    trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:, 0]))
-    print('Train Score: %.2f RMSE' % (trainScore))
-    testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:, 0]))
-    print('Test Score: %.2f RMSE' % (testScore))
-
-    return trainPredict, testPredict, dataset, scaler
+    return train_predict, test_predict, dataset, scaler
 
 
-def plot(trainPredict, testPredict, dataset, scaler):
-    trainPredictPlot = numpy.empty_like(dataset)
-    trainPredictPlot[:, :] = numpy.nan
-    trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+def plot(train_predict, test_predict, dataset, scaler):
+    train_predict_plot = numpy.empty_like(dataset)
+    train_predict_plot[:, :] = numpy.nan
+    train_predict_plot[look_back:len(train_predict)+look_back, :] = train_predict
     # shift test predictions for plotting
-    testPredictPlot = numpy.empty_like(dataset)
-    testPredictPlot[:, :] = numpy.nan
-    testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-    # plot baseline and predictions
-    #plt.plot(trainPredictPlot)
-    #plt.plot(testPredictPlot)
+    test_predict_plot = numpy.empty_like(dataset)
+    test_predict_plot[:, :] = numpy.nan
+    test_predict_plot[len(train_predict)+(look_back*2)+1:len(dataset)-1, :] = test_predict
     
-    print(trainPredictPlot)
-
-    return trainPredictPlot, testPredictPlot
+    # Return data to be used in UI line graph
+    return train_predict_plot, test_predict_plot  
